9a80703119463b6e8977f7c12deabdee3f7d597d
Vinicius Pinheiro Sátiro <viniciussatiro2@gmail.com>
2025-02-08 21:05:51 -0300

    ajustando numeros mágicos

--- src/interacoes.js
+++ src/interacoes.js
@@ -1,13 +1,18 @@
 import { atualizaPersonagem } from "./personagem"
 
 export function dormir (personagem, tempo){
-    if(tempo < 0){
+    const TEMPO_INVALIDO = 0;
+    if(tempo < TEMPO_INVALIDO){
         console.log("Insira um tempo válido")
     }
-    const tempoDormido = tempo/5
-    let pontosEnergia = tempoDormido*4
+
+    const INTERVALO_SONO = 5
+    const ENERGIA_POR_INTERVALO = 4
+    const BONUS_ENERGIA = 2
+    const tempoDormido = tempo/INTERVALO_SONO
+    let pontosEnergia = tempoDormido*ENERGIA_POR_INTERVALO
     for (let i = 1; i < tempoDormido; i++) {
-        pontosEnergia+=2
+        pontosEnergia+=BONUS_ENERGIA
     }
 
     personagem.energia += pontosEnergia
